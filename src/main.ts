6562c6550ce5153089b096cb0d6f5fb934acf46c
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-07-03 21:08:36 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,67 @@
-// solution goes here
-//<article class="image-card">
-//<h2 class="title">Title of image goes here</h2>
-//<img src="./assets/image-placeholder.jpg" class="image" />
-//<div class="likes-section">
-//  <span class="likes">0 likes</span>
-//  <button class="like-button">♥</button>
-//</div>
-//<ul class="comments">
-//  <li>Get rid of these comments</li>
- // <li>And replace them with the real ones</li>
- // <li>From the server</li>
-//</ul>
-//</article>
+let state = {
+    images: []
+}
+function getImagesFromServer() {
 
-function renderCard(){
-    let imageContainer:any =document.querySelector('.image-container')
-    imageContainer.textContent=''
+}
 
-    let articleEl=document.createElement('article')
-    articleEl.className='image-card'
+function renderCard() {
+    // solution goes here
+    //<article class="image-card">
+    //<h2 class="title">Title of image goes here</h2>
+    //<img src="./assets/image-placeholder.jpg" class="image" />
+    //<div class="likes-section">
+    //  <span class="likes">0 likes</span>
+    //  <button class="like-button">♥</button>
+    //</div>
+    //<ul class="comments">
+    //  <li>Get rid of these comments</li>
+    // <li>And replace them with the real ones</li>
+    // <li>From the server</li>
+    //</ul>
+    //</article>
 
-    let h2El=document.createElement('h2')
-    h2El.className='tittle'
-    h2El.textContent='Title of image goes here'
+    let imageContainer: any = document.querySelector('.image-container')
+    imageContainer.textContent = ''
 
-    let imgEl=document.createElement('img')
-    imgEl.className='image'
-    imgEl.src='./assets/image-placeholder.jpg'
+    let articleEl = document.createElement('article')
+    articleEl.className = 'image-card'
 
-    let divEl=document.createElement('div')
-    divEl.className='likes-section'
+    let h2El = document.createElement('h2')
+    h2El.className = 'tittle'
+    h2El.textContent = 'Title of image goes here'
 
-    let spanEl=document.createElement('span')
-    spanEl.className='likes'
-    spanEl.textContent='0 likes'
+    let imgEl = document.createElement('img')
+    imgEl.className = 'image'
+    imgEl.src = './assets/image-placeholder.jpg'
 
-    let buttonEl=document.createElement('button')
-    buttonEl.className='like-button'
-    buttonEl.textContent='♥'
+    let divEl = document.createElement('div')
+    divEl.className = 'likes-section'
 
-    let ulEl=document.createElement('ul')
-    ulEl.className='comments'
+    let spanEl = document.createElement('span')
+    spanEl.className = 'likes'
+    spanEl.textContent = '0 likes'
 
-    let liEl=document.createElement('li')
-    liEl.textContent='Get rid of these comments'
+    let buttonEl = document.createElement('button')
+    buttonEl.className = 'like-button'
+    buttonEl.textContent = '♥'
 
-    let liEl2=document.createElement('li')
-    liEl2.textContent='And replace them with the real ones'
+    let ulEl = document.createElement('ul')
+    ulEl.className = 'comments'
 
-    let liEl3=document.createElement('li')
-    liEl3.textContent='From the server'
+    let liEl = document.createElement('li')
+    liEl.textContent = 'Get rid of these comments'
 
-divEl.append(spanEl, buttonEl)
-ulEl.append(liEl, liEl2, liEl3)
-articleEl.append(h2El, imgEl, divEl, ulEl)
-imageContainer.append(articleEl)
+    let liEl2 = document.createElement('li')
+    liEl2.textContent = 'And replace them with the real ones'
+
+    let liEl3 = document.createElement('li')
+    liEl3.textContent = 'From the server'
+
+    divEl.append(spanEl, buttonEl)
+    ulEl.append(liEl, liEl2, liEl3)
+    articleEl.append(h2El, imgEl, divEl, ulEl)
+    imageContainer.append(articleEl)
 }
 
 console.log('yay')
