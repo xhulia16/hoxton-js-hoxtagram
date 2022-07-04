8916657c4aa04bb18a48c0972bd384f58d56c527
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-07-04 15:01:36 +0200

    state

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,17 @@ type State = {
     images: Image[]
 }
 
+let banana = [2, 4, 6]
 
 let state: State = {
     images: []
 }
+
+console.log(state.images)
+console.log('hi')
+
 function getImagesFromServer() {
-    fetch("http://localhost:3000/images")
+    fetch("http://localhost:5000/images")
         .then(resp => resp.json())
         .then(imagesFromServer => { state.images = imagesFromServer })
     renderCard()
@@ -49,41 +54,40 @@ function renderCard() {
 
     for (let image of state.images) {
 
-        let articleEl = document.createElement('article')
-        articleEl.className = 'image-card'
+    let articleEl = document.createElement('article')
+    articleEl.className = 'image-card'
 
-        let h2El = document.createElement('h2')
-        h2El.className = 'tittle'
-        h2El.textContent = 'Title of image goes here'
+    let h2El = document.createElement('h2')
+    h2El.className = 'tittle'
+    h2El.textContent = 'Title of image goes here'
 
-        let imgEl = document.createElement('img')
-        imgEl.className = 'image'
-        imgEl.src = image.image
+    let imgEl = document.createElement('img')
+    imgEl.className = 'image'
+    imgEl.src = "./assets/image-placeholder.jpg"
 
-        let divEl = document.createElement('div')
-        divEl.className = 'likes-section'
+    let divEl = document.createElement('div')
+    divEl.className = 'likes-section'
 
-        let spanEl = document.createElement('span')
-        spanEl.className = 'likes'
-        spanEl.textContent = '0 likes'
+    let spanEl = document.createElement('span')
+    spanEl.className = 'likes'
+    spanEl.textContent = '0 likes'
 
-        let buttonEl = document.createElement('button')
-        buttonEl.className = 'like-button'
-        buttonEl.textContent = '♥'
+    let buttonEl = document.createElement('button')
+    buttonEl.className = 'like-button'
+    buttonEl.textContent = '♥'
 
-        let ulEl = document.createElement('ul')
-        ulEl.className = 'comments'
+    let ulEl = document.createElement('ul')
+    ulEl.className = 'comments'
 
-        let liEl = document.createElement('li')
-        liEl.textContent = 'Get rid of these comments'
+    let liEl = document.createElement('li')
+    liEl.textContent = 'Get rid of these comments'
 
-        divEl.append(spanEl, buttonEl)
-        ulEl.append(liEl)
-        articleEl.append(h2El, imgEl, divEl, ulEl)
-        imageContainer.append(articleEl)
-    }
-}
-
+    divEl.append(spanEl, buttonEl)
+    ulEl.append(liEl)
+    articleEl.append(h2El, imgEl, divEl, ulEl)
+    imageContainer.append(articleEl)
+}}
 getImagesFromServer()
 renderCard()
+
 
