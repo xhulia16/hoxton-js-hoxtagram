3823d0b53505abfbf0cc8ab76c18d9a8887f5142
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-07-05 10:01:19 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -28,8 +28,10 @@ console.log('hi')
 function getImagesFromServer() {
     fetch("http://localhost:4000/images")
         .then(resp => resp.json())
-        .then(imagesFromServer => { state.images = imagesFromServer 
-    render()})
+        .then(imagesFromServer => {
+            state.images = imagesFromServer
+            render()
+        })
 }
 
 function render() {
@@ -54,39 +56,43 @@ function render() {
 
     for (let image of state.images) {
 
-    let articleEl = document.createElement('article')
-    articleEl.className = 'image-card'
+        let articleEl = document.createElement('article')
+        articleEl.className = 'image-card'
 
-    let h2El = document.createElement('h2')
-    h2El.className = 'tittle'
-    h2El.textContent = 'Title of image goes here'
+        let h2El = document.createElement('h2')
+        h2El.className = 'tittle'
+        h2El.textContent = image.title
 
-    let imgEl = document.createElement('img')
-    imgEl.className = 'image'
-    imgEl.src = "./assets/image-placeholder.jpg"
+        let imgEl = document.createElement('img')
+        imgEl.className = 'image'
+        imgEl.src = image.image
 
-    let divEl = document.createElement('div')
-    divEl.className = 'likes-section'
+        let divEl = document.createElement('div')
+        divEl.className = 'likes-section'
 
-    let spanEl = document.createElement('span')
-    spanEl.className = 'likes'
-    spanEl.textContent = '0 likes'
+        let spanEl = document.createElement('span')
+        spanEl.className = 'likes'
+        spanEl.textContent = `${image.likes} likes`
 
-    let buttonEl = document.createElement('button')
-    buttonEl.className = 'like-button'
-    buttonEl.textContent = '♥'
+        let buttonEl = document.createElement('button')
+        buttonEl.className = 'like-button'
+        buttonEl.textContent = '♥'
 
-    let ulEl = document.createElement('ul')
-    ulEl.className = 'comments'
+        let ulEl = document.createElement('ul')
+        ulEl.className = 'comments'
 
-    let liEl = document.createElement('li')
-    liEl.textContent = 'Get rid of these comments'
+        for (let comment of image.comments) {
 
-    divEl.append(spanEl, buttonEl)
-    ulEl.append(liEl)
-    articleEl.append(h2El, imgEl, divEl, ulEl)
-    imageContainer.append(articleEl)
-}}
+            let liEl = document.createElement('li')
+            liEl.textContent = comment.content
+            ulEl.append(liEl)
+        }
+
+        divEl.append(spanEl, buttonEl)
+        articleEl.append(h2El, imgEl, divEl, ulEl)
+        imageContainer.append(articleEl)
+    }
+}
 getImagesFromServer()
 render()
 
